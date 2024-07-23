3f1ce947d04e3b046c6f999a27e235a3396b2336
KdevG <toni.mrtnz.cn@gmail.com>
2024-07-23 10:40:42 +0200

    Ajustes Interfaz

--- reminder_gpt/reminder_gpt/settings.py
+++ reminder_gpt/reminder_gpt/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / "static"]
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
