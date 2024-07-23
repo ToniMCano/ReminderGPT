3a43685503b52c2234a9ebf5743f8a639fd85868
HomeX <homex@localhost.localdomain>
2024-07-23 09:24:28 +0200

    Estructura CRUD desde el FrontEnd OK. Faltan detalles y estética

--- reminder_gpt/pages/models.py
+++ reminder_gpt/pages/models.py
@@ -7,10 +7,11 @@ from django.core.exceptions import ValidationError
 
 
 class Registry(models.Model):
-
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     project_name = models.CharField(verbose_name = "Nombre del Proyecto", max_length=50)
     user = models.ForeignKey(User , verbose_name = "Usuario" , on_delete = models.CASCADE , default = None)
-    Log = models.TextField(verbose_name = "Log" , default = None)
+    log = models.TextField(verbose_name = "Log" , default = None)
     #email = models.EmailField(max_length=254 , unique = True , default = "provwork@gmail.com") 
     #phone = models.CharField(verbose_name="Teléfono", max_length=9, validators=[validate_phone] , unique = True ,  default = 123456789)  
     created = models.DateField(auto_now_add = True , verbose_name = 'Fecha de Creación')
--- reminder_gpt/pages/views.py
+++ reminder_gpt/pages/views.py
@@ -1,8 +1,11 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView , UpdateView , DeleteView
+from django.urls import reverse , reverse_lazy
+
 from .models import Registry
 
 
@@ -25,3 +28,36 @@ class PagesListView(ListView):
 class PagesDetailView(DetailView):
     
     model = Registry
+    
+
+class PageCreate(CreateView):
+    
+    model = Registry
+    
+    fields = ["project_name" , "log"]
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # Establece el usuario al usuario que ha iniciado sesión
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('pages:pages_home')
+
+    
+class PageUpdate(UpdateView):
+    
+    model = Registry
+    
+    fields = ["project_name" , "log"]
+    
+    template_name_suffix = "_update_form"
+    
+    def get_success_url(self):
+        return reverse_lazy("pages:update" , args = [self.object.id]) + '?ok'
+    
+    
+class PageDelete(DeleteView):
+    
+    model = Registry
+    
+    success_url =  reverse_lazy('pages:pages_home')
